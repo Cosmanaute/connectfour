5d5ac60b8446052e94a0e1a61a3756151504d6a5
cuebin <beknosee@outlook.com>
2023-04-07 16:51:16 +0200

    README.md
    added README.md file as well as some comments in the code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Connectfour in rust
+
+This was made using OOP in rust.
+
+## How to play
+
+The game consists of two players: O and X. 
+To choose which column to move to, 
+you just input a number between 1 and 7. 
+1 is far left and 7 is far right. 
+
+4 connecting pieces including diagonals == win.
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ impl Board {
         }
     }
     
+    //iterates backwards(from bottom) in a column to check if any cell is empty and places mark in cell if so
     fn place_mark(&mut self, col: usize, mark: char) {
         for row in self.grid.iter_mut().rev() {
             if row[col] == '#' {
@@ -31,16 +32,20 @@ impl Board {
             }
         } 
     }
+
     fn check_for_win(&self, mark: char) -> bool {
         for i in 0..6 {
         for j in 0..7 {
             if self.grid[i][j] == mark {
+                //checks horizontal
                 if j <= 3 && self.grid[i][j+1] == mark && self.grid[i][j+2] == mark && self.grid[i][j+3] == mark {
                     return true;
                 }
+                //checks vertical
                 if i <= 2 && self.grid[i+1][j] == mark &&self.grid[i+2][j] == mark && self.grid[i+3][j] == mark {
                     return true;
                 }
+                //checks diagonal
                 if i <= 2 && j <= 3 && self.grid[i+1][j+1] == mark && self.grid[i+2][j+2] == mark && self.grid[i+3][j+3] == mark {
                     return true;
                 }
@@ -51,8 +56,7 @@ impl Board {
         }
     }
     return false;
-}
-
+    }
 }
 
 fn main() {
@@ -60,23 +64,27 @@ fn main() {
     let mut board = Board::new();
     let mut current_player = 'O';
 
+    //sends escape sequence that clears the terminal
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); 
     
     loop {
+
         board.display();
         
-        //get input
+        //gets input
         print!("Player {}, enter your move: ", current_player);
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Could not read line");
 
+        //checks if you inputet more than one character
         if input.len() != 2 {
             print!("{esc}[2J{esc}[1;1H", esc = 27 as char); 
             println!("Invalid input");
             continue;
         }
 
+        //parses the input(string) to a usize(u64/u32)
         let col: usize = input.trim().parse().unwrap();
 
         board.place_mark(col - 1, current_player);
@@ -96,6 +104,5 @@ fn main() {
             current_player = 'X';
             print!("{esc}[2J{esc}[1;1H", esc = 27 as char); 
         }
-
     }
 }
